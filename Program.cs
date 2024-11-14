5c53ee08edfbf6da415734713c2eba7f78c440ac
Marcel-bit <49585259+Marcel-bit@users.noreply.github.com>
2024-11-14 06:29:28 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static ConsoleApp1.Class1;
+
+namespace TestProgram
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello");
+			HelloInProgram.Hello();
+			Hello();
+			Console.ReadKey();
+			
+		}
+	}
+
+	class HelloInProgram
+	{
+		public static void Hello()//public makes it visible/usable to outside its class
+		{
+			Console.WriteLine("Hello1");
+		}
+	}
+}
